564df03103f77537589672a734fda5b54b376666
Matteo <mateus.sakata@gmail.com>
2022-02-09 01:54:07 -0600

    Add youtube video support

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key}")
 desired_cols = ["copyright", "url"]
 
 if response:
-  raw_data = json.loads(response.text)
-  data = {k: v for k, v in raw_data.items() if k in desired_cols}
-  subs = jinja2.Environment( 
-              loader=jinja2.FileSystemLoader('./')      
-              ).get_template('template.jinja').render(**data)
-  # lets write the substitution to a file
-  with open("apod-displayer/index.html",'w') as f:
-    f.write(subs)
-  
+    raw_data = json.loads(response.text)
+    data = {k: v for k, v in raw_data.items() if k in desired_cols}
+    subs = (
+        jinja2.Environment(loader=jinja2.FileSystemLoader("./"))
+        .get_template("template.jinja")
+        .render(**data)
+    )
+    # lets write the substitution to a file
+    with open("apod-displayer/index.html", "w") as f:
+        f.write(subs)
